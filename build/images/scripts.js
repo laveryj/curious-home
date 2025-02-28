76082b6e965e12c9559179cfa1a4e7c673bb015d
laveryj <105878149+laveryj@users.noreply.github.com>
2025-02-28 15:56:53 +0000

    Auto-commit: Build updated by VSCode

--- build/images/scripts.js
+++ build/images/scripts.js
@@ -9,8 +9,9 @@ document.addEventListener("DOMContentLoaded", function () {
       return console.error("Error loading ".concat(section, ":"), error);
     });
   });
-}); // ==== pricing plan toggler
+});
 
+// ==== pricing plan toggler
 var togglePlan = document.querySelector("#togglePlan");
 var toggle = document.getElementById("togglePlan");
 document.querySelector(".monthly").addEventListener("click", function () {
@@ -26,8 +27,9 @@ toggle.addEventListener("change", function () {
   document.querySelectorAll(".price-annual, .annual-label").forEach(function (el) {
     return el.classList.toggle("hidden");
   });
-}); // ==== for menu scroll
+});
 
+// ==== for menu scroll
 var pageLink = document.querySelectorAll(".menu-scroll");
 pageLink.forEach(function (elem) {
   elem.addEventListener("click", function (e) {
@@ -37,18 +39,17 @@ pageLink.forEach(function (elem) {
       offsetTop: 1 - 60
     });
   });
-}); // section menu active
+});
 
+// section menu active
 function onScroll(event) {
   var sections = document.querySelectorAll(".menu-scroll");
   var scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
-
   for (var i = 0; i < sections.length; i++) {
     var currLink = sections[i];
     var val = currLink.getAttribute("href");
     var refElement = document.querySelector(val);
     var scrollTopMinus = scrollPos + 73;
-
     if (refElement.offsetTop <= scrollTopMinus && refElement.offsetTop + refElement.offsetHeight > scrollTopMinus) {
       document.querySelector(".menu-scroll").classList.remove("active");
       currLink.classList.add("active");
@@ -57,9 +58,7 @@ function onScroll(event) {
     }
   }
 }
-
 window.document.addEventListener("scroll", onScroll);
-
 function initAcc(elem, option) {
   document.addEventListener("click", function (e) {
     if (!e.target.matches(elem + " .faq-btn")) return;else {
@@ -70,20 +69,18 @@ function initAcc(elem, option) {
             e.classList.remove("active");
           });
         }
-
         e.target.parentElement.classList.add("active");
       } else {
         e.target.parentElement.classList.remove("active");
       }
     }
   });
-} //activate accordion function
+}
 
-
+//activate accordion function
 initAcc(".faqs", true);
 document.addEventListener("DOMContentLoaded", function () {
   var backToTop = document.querySelector(".back-to-top");
-
   if (backToTop) {
     // Scroll event to show/hide button
     window.addEventListener("scroll", function () {
@@ -92,8 +89,9 @@ document.addEventListener("DOMContentLoaded", function () {
       } else {
         backToTop.classList.add("hidden");
       }
-    }); // Click event to scroll to top smoothly
+    });
 
+    // Click event to scroll to top smoothly
     backToTop.addEventListener("click", function () {
       window.scrollTo({
         top: 0,
